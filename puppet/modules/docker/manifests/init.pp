63822d044fac4a94d7fb155fa99fa6757898b68a
Rohith <gambol99@gmail.com>
2014-10-07 18:43:38 +0100

    [docker] - added a temporary docker module

---
+++ puppet/modules/docker/manifests/init.pp
@@ -0,0 +1,12 @@
+#
+#   Author: Rohith (gambol99@gmail.com)
+#   Date: 2014-10-07 18:36:25 +0100 (Tue, 07 Oct 2014)
+#
+#  vim:ts=2:sw=2:et
+#
+class docker(
+  $version      = 'installed',
+  $package_name = 'docker.io',
+) {
+  class { 'install': }
+}
---
+++ puppet/modules/docker/manifests/install.pp
@@ -0,0 +1,13 @@
+#
+#   Author: Rohith (gambol99@gmail.com)
+#   Date: 2014-10-07 18:36:30 +0100 (Tue, 07 Oct 2014)
+#
+#  vim:ts=2:sw=2:et
+#
+class docker::install {
+  package {
+    'docker':
+      ensure => $docker::version,
+      name   => $docker::package_name,
+  }
+}
